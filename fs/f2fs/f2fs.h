2a5545efc484b7c6830046b49abc200417a9812d
dennes544 <dennes5443@gmail.com>
2014-07-23 03:26:10 -0400

    F2FS: Upstream updates f2fs: switch to iov_iter_alignment() f2fs: check name_len of dir entry to prevent from deadloop f2fs: avoid to access NULL pointer in issue_flush_thread f2fs: introduce help function {create,destroy}_flush_cmd_control f2fs: cleanup the needless return of f2fs_create_root_stats f2fs: remove the unused stat_lock

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -368,12 +368,6 @@ struct f2fs_sm_info {
 	unsigned int min_ipu_util;	/* in-place-update threshold */
 
 	/* for flush command control */
-	struct task_struct *f2fs_issue_flush;	/* flush thread */
-	wait_queue_head_t flush_wait_queue;	/* waiting queue for wake-up */
-	struct flush_cmd *issue_list;		/* list for command issue */
-	struct flush_cmd *dispatch_list;	/* list for command dispatch */
-	spinlock_t issue_lock;			/* for issue list lock */
-	struct flush_cmd *issue_tail;		/* list tail of issue list */
 	struct flush_cmd_control *cmd_control_info;
 };
 
@@ -1345,7 +1339,6 @@ bool space_for_roll_forward(struct f2fs_sb_info *);
 struct f2fs_stat_info {
 	struct list_head stat_list;
 	struct f2fs_sb_info *sbi;
-	struct mutex stat_lock;
 	int all_area_segs, sit_area_segs, nat_area_segs, ssa_area_segs;
 	int main_area_segs, main_area_sections, main_area_zones;
 	int hit_ext, total_ext;
