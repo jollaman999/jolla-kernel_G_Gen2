a4f2ddb5593888eec6662987f43f7ffd23088266
jollaman999 <admin@jollaman999.com>
2014-08-22 16:17:50 +0900

    clock-8960: Fix gpu clock unable to go 607.5MHz

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -3535,9 +3535,11 @@ static struct clk_freq_tbl clk_tbl_gfx3d[] = {
 	F_GFX3D(266667000, pll2,  1,  3),
 	F_GFX3D(320000000, pll2,  2,  5),
 	F_GFX3D(400000000, pll2,  1,  2),
-	F_GFX3D(450000000, pll15, 1,  2),
 // jollaman999
 // GPU Overclock
+#ifndef CONFIG_GPU_OVERCLOCK
+	F_GFX3D(450000000, pll15, 1,  2),
+#endif /* CONFIG_GPU_OVERCLOCK */
 #ifdef CONFIG_GPU_OVERCLOCK
 	F_GFX3D(533000000, pll2,  2,  3),
 	F_GFX3D(607500000, pll15, 1,  2),
