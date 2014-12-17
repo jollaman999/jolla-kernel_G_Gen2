66719021fce2aa157039b231561af3ee9d9093c6
jollaman999 <admin@jollaman999.com>
2014-12-17 11:41:14 +0900

    f2fs: Fix logic errors Part_1

--- fs/f2fs/inline.c
+++ fs/f2fs/inline.c
@@ -264,15 +264,8 @@ process_inline:
 	return false;
 }
 
-/*
 struct f2fs_dir_entry *find_in_inline_dir(struct inode *dir,
 				struct qstr *name, struct page **res_page)
-*/
-struct f2fs_dir_entry *find_in_inline_dir(struct inode *dir,
-				struct page *dentry_page,
-				struct qstr *name, int *max_slots,
-				struct page **res_page,
-				bool nocase)
 {
 	struct f2fs_sb_info *sbi = F2FS_SB(dir->i_sb);
 	struct f2fs_inline_dentry *inline_dentry;
@@ -287,7 +280,7 @@ struct f2fs_dir_entry *find_in_inline_dir(struct inode *dir,
 	inline_dentry = inline_data_addr(ipage);
 
 	make_dentry_ptr(&d, (void *)inline_dentry, 2);
-	de = find_target_dentry(dentry_page, name, NULL, res_page, &d, nocase);
+	de = find_target_dentry(name, NULL, &d);
 
 	unlock_page(ipage);
 	if (de)
