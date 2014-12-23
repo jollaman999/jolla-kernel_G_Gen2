3df5a005331f37b65ed9e01efaf0f2e278aea405
Herbert Xu <herbert@gondor.apana.org.au>
2014-12-23 16:40:18 +1100

    hwrng: core - Fix current_rng init/cleanup race yet again
    The kref solution is still buggy because we were only focusing
    on the register/unregister race.  The same race affects the
    setting of current_rng through sysfs.
    
    This patch fixes it by using kref_get_unless_zero.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    
    Conflicts:
    	drivers/char/hw_random/core.c

--- drivers/char/hw_random/core.c
+++ drivers/char/hw_random/core.c
@@ -105,7 +105,6 @@ static inline void cleanup_rng(struct kref *kref)
 static void set_current_rng(struct hwrng *rng)
 {
 	BUG_ON(!mutex_is_locked(&rng_mutex));
-	kref_get(&rng->ref);
 	current_rng = rng;
 }
 
@@ -152,11 +151,19 @@ static inline int hwrng_init(struct hwrng *rng)
 {
 	int err;
 
+	if (kref_get_unless_zero(&rng->ref))
+		goto skip_init;
+
 	if (rng->init) {
 		err = rng->init(rng);
 		if (err)
 			return err;
 	}
+
+	kref_init(&rng->ref);
+	reinit_completion(&rng->cleanup_done);
+
+skip_init:
 
 	current_quality = rng->quality ? : default_quality;
 	current_quality &= 1023;
@@ -466,6 +473,9 @@ int hwrng_register(struct hwrng *rng)
 			goto out_unlock;
 	}
 
+	init_completion(&rng->cleanup_done);
+	complete(&rng->cleanup_done);
+
 	old_rng = current_rng;
 	err = 0;
 	if (!old_rng) {
@@ -492,8 +502,6 @@ int hwrng_register(struct hwrng *rng)
 		 */
 		add_early_randomness(rng);
 	}
-
-	init_completion(&rng->cleanup_done);
 
 out_unlock:
 	mutex_unlock(&rng_mutex);
