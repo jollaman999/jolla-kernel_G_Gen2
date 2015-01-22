xwsa568/kernel_msm

Adjust PLAYBACK_PERIOD_SIZE
This now agrees on what is defined in the HAL.
Without this, NuPlayer will sometimes behave strange and fail to fill up the buffer (while jerking the video playback)

Conflicts:
	sound/soc/msm/msm-pcm-q6.c

sound/soc/msm/msm-pcm-q6.c
@@ -40,7 +40,7 @@ struct snd_msm {
 };
 
 #define PLAYBACK_NUM_PERIODS	8
-#define PLAYBACK_PERIOD_SIZE	4096
+#define PLAYBACK_PERIOD_SIZE	3840 /* this is msm8960/platform.h:DEEP_BUFFER_OUTPUT_PERIOD_SIZE x 4 */
 #define CAPTURE_NUM_PERIODS	2
 #define CAPTURE_MAX_PERIOD_SIZE 4096
 #define CAPTURE_MIN_PERIOD_SIZE 320

jollaman999/jolla-kernel_G_Gen2

sound/soc/msm/msm-pcm-q6.c
@@ -40,7 +40,7 @@ struct snd_msm {
 };
 
 #define PLAYBACK_NUM_PERIODS		8
-#define PLAYBACK_MAX_PERIOD_SIZE	4096
+#define PLAYBACK_MAX_PERIOD_SIZE	3840 /* this is msm8960/platform.h:DEEP_BUFFER_OUTPUT_PERIOD_SIZE x 4 */
 #define PLAYBACK_MIN_PERIOD_SIZE	1024
 #define CAPTURE_NUM_PERIODS		2
 #define CAPTURE_MAX_PERIOD_SIZE		4096