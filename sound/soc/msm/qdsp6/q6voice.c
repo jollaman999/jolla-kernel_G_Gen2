2ac5b551683a9b9e5a47020ecb05c445f9584ebf
jollaman999 <admin@jollaman999.com>
2015-02-01 01:53:19 +0900

    Revert "ASoC: msm: qdsp6v2: Fix buffer overflow in voice driver"
    This reverts commit 7a8c1aa3a21b1fc4e1b79b924a938612cdbc9e66.

--- sound/soc/msm/qdsp6/q6voice.c
+++ sound/soc/msm/qdsp6/q6voice.c
@@ -1314,8 +1314,7 @@ static int voice_send_cvs_register_cal_cmd(struct voice_data *v)
 
 	/* get the cvs cal data */
 	get_all_vocstrm_cal(&cal_block);
-	if (cal_block.cal_size == 0 ||
-	    cal_block.cal_size > CVS_CAL_SIZE)
+	if (cal_block.cal_size == 0)
 		goto fail;
 
 	if (v == NULL) {
@@ -1390,8 +1389,7 @@ static int voice_send_cvs_deregister_cal_cmd(struct voice_data *v)
 	u16 cvs_handle;
 
 	get_all_vocstrm_cal(&cal_block);
-	if (cal_block.cal_size == 0 ||
-	    cal_block.cal_size > CVS_CAL_SIZE)
+	if (cal_block.cal_size == 0)
 		return 0;
 
 	if (v == NULL) {
@@ -1447,8 +1445,7 @@ static int voice_send_cvp_map_memory_cmd(struct voice_data *v)
 
 	/* get all cvp cal data */
 	get_all_cvp_cal(&cal_block);
-	if (cal_block.cal_size == 0 ||
-	    cal_block.cal_size > CVP_CAL_SIZE)
+	if (cal_block.cal_size == 0)
 		goto fail;
 
 	if (v == NULL) {
@@ -1520,8 +1517,7 @@ static int voice_send_cvp_unmap_memory_cmd(struct voice_data *v)
 	uint32_t cal_paddr = 0;
 
 	get_all_cvp_cal(&cal_block);
-	if (cal_block.cal_size == 0 ||
-	    cal_block.cal_size > CVP_CAL_SIZE)
+	if (cal_block.cal_size == 0)
 		return 0;
 
 	if (v == NULL) {
@@ -1589,8 +1585,7 @@ static int voice_send_cvs_map_memory_cmd(struct voice_data *v)
 
 	/* get all cvs cal data */
 	get_all_vocstrm_cal(&cal_block);
-	if (cal_block.cal_size == 0 ||
-	    cal_block.cal_size > CVS_CAL_SIZE)
+	if (cal_block.cal_size == 0)
 		goto fail;
 
 	if (v == NULL) {
@@ -1662,8 +1657,7 @@ static int voice_send_cvs_unmap_memory_cmd(struct voice_data *v)
 	uint32_t cal_paddr = 0;
 
 	get_all_vocstrm_cal(&cal_block);
-	if (cal_block.cal_size == 0 ||
-	    cal_block.cal_size > CVS_CAL_SIZE)
+	if (cal_block.cal_size == 0)
 		return 0;
 
 	if (v == NULL) {
@@ -1732,8 +1726,7 @@ static int voice_send_cvp_register_cal_cmd(struct voice_data *v)
 
       /* get the cvp cal data */
 	get_all_vocproc_cal(&cal_block);
-	if (cal_block.cal_size == 0 ||
-	    cal_block.cal_size > CVP_CAL_SIZE)
+	if (cal_block.cal_size == 0)
 		goto fail;
 
 	if (v == NULL) {
@@ -1808,8 +1801,7 @@ static int voice_send_cvp_deregister_cal_cmd(struct voice_data *v)
 	u16 cvp_handle;
 
 	get_all_vocproc_cal(&cal_block);
-	if (cal_block.cal_size == 0 ||
-	    cal_block.cal_size > CVP_CAL_SIZE)
+	if (cal_block.cal_size == 0)
 		return 0;
 
 	if (v == NULL) {
@@ -1859,7 +1851,6 @@ static int voice_send_cvp_register_vol_cal_table_cmd(struct voice_data *v)
 	struct cvp_register_vol_cal_table_cmd cvp_reg_cal_tbl_cmd;
 	struct acdb_cal_block vol_block;
 	struct acdb_cal_block voc_block;
-	struct acdb_cal_block cvp_block;
 	int ret = 0;
 	void *apr_cvp;
 	u16 cvp_handle;
@@ -1869,10 +1860,8 @@ static int voice_send_cvp_register_vol_cal_table_cmd(struct voice_data *v)
 	/* get the cvp vol cal data */
 	get_all_vocvol_cal(&vol_block);
 	get_all_vocproc_cal(&voc_block);
-	get_all_cvp_cal(&cvp_block);
 
-	if (vol_block.cal_size == 0 ||
-	    cvp_block.cal_size > CVP_CAL_SIZE)
+	if (vol_block.cal_size == 0)
 		goto fail;
 
 	if (v == NULL) {
@@ -1944,16 +1933,12 @@ static int voice_send_cvp_deregister_vol_cal_table_cmd(struct voice_data *v)
 {
 	struct cvp_deregister_vol_cal_table_cmd cvp_dereg_cal_tbl_cmd;
 	struct acdb_cal_block cal_block;
-	struct acdb_cal_block voc_block;
 	int ret = 0;
 	void *apr_cvp;
 	u16 cvp_handle;
 
 	get_all_vocvol_cal(&cal_block);
-	get_all_cvp_cal(&voc_block);
-
-	if (cal_block.cal_size == 0 ||
-	    voc_block.cal_size > CVP_CAL_SIZE)
+	if (cal_block.cal_size == 0)
 		return 0;
 
 	if (v == NULL) {
