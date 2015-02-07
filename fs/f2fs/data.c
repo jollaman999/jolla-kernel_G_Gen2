90ca8235c881f59c2208eaad74d03f77d27cd92c
jollaman999 <admin@jollaman999.com>
2015-02-07 22:00:05 +0900

    Revert "mm: change invalidatepage prototype to accept length"
    This reverts commit 0b7f3d02fa3b6206493696d594c5679b6994b831.

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -1107,8 +1107,7 @@ static ssize_t f2fs_direct_IO(int rw, struct kiocb *iocb,
 	return err;
 }
 
-static void f2fs_invalidate_data_page(struct page *page, unsigned int offset,
-				      unsigned int length)
+static void f2fs_invalidate_data_page(struct page *page, unsigned long offset)
 {
 	struct inode *inode = page->mapping->host;
 
