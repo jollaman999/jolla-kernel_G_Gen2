728e1ebe3cfc3bbd0080b2c8a61c2b578f890f8c
jollaman999 <admin@jollaman999.com>
2015-02-07 22:09:21 +0900

    Revert "f2fs: fix typos for the word "destroy" in jump labels"
    This reverts commit 27f17b5f05f57739a0276c98301f0851b4211afe.

--- fs/f2fs/node.c
+++ fs/f2fs/node.c
@@ -2105,17 +2105,17 @@ int __init create_node_manager_caches(void)
 	free_nid_slab = f2fs_kmem_cache_create("free_nid",
 			sizeof(struct free_nid));
 	if (!free_nid_slab)
-		goto destroy_nat_entry;
+		goto destory_nat_entry;
 
 	nat_entry_set_slab = f2fs_kmem_cache_create("nat_entry_set",
 			sizeof(struct nat_entry_set));
 	if (!nat_entry_set_slab)
-		goto destroy_free_nid;
+		goto destory_free_nid;
 	return 0;
 
-destroy_free_nid:
+destory_free_nid:
 	kmem_cache_destroy(free_nid_slab);
-destroy_nat_entry:
+destory_nat_entry:
 	kmem_cache_destroy(nat_entry_slab);
 fail:
 	return -ENOMEM;
