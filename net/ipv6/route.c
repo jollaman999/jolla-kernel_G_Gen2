cbc021b04cbf02bb5d1b82cee9d0b8c84338e31a
jollaman999 <admin@jollaman999.com>
2015-03-01 18:49:19 +0900

    ipv6: Fix incompatible type build warning

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -618,7 +618,7 @@ int rt6_route_rcv(struct net_device *dev, u8 *opt, int len,
 	}
 
 	if (rinfo->prefix_len == 0)
-		rt = rt6_get_dflt_router(gwaddr, net);
+		rt = rt6_get_dflt_router(gwaddr, dev);
 	else
 		rt = rt6_get_route_info(net, prefix, rinfo->prefix_len,
 					gwaddr, dev->ifindex);
